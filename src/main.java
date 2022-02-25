eb9eeec0abd74a4c864d2442e98c7197b90b7b8e
yousef.omd <yousefomidali78@gmail.com>
2022-02-25 22:26:24 +0330

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,53 @@
+import java.security.KeyStore;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class main {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList();
+        words.add("Amir");
+        words.add("Hatef");
+        words.add("Mehran");
+        words.add("Mojtaba");
+        words.add("Mohammad");
+        words.add("Davood");
+        words.add("Reza");
+        words.add("Mohsen");
+        words.add("Yousef");
+        //-----------------------------------------------------------------------------------
+        //a
+        Map<Integer, List<String>> charFrequency =
+                words.stream()
+                        .collect(Collectors.groupingBy(String::length));
+        System.out.println(charFrequency);
+        //------------------------------------------------------------------------------------
+        //b
+//        Map<Integer, List<String>> charFrequency1 =
+//                words.stream()
+//                        .collect(Collectors.groupingBy(String::length)).entrySet()
+//                        .stream().collect(Collectors.toMap(Map.Entry::getKey,);
+//        System.out.println(charFrequency1);
+        //------------------------------------------------------------------------------------
+        //Additional
+        Map<Character, Long> charFrequency2 = words.stream()
+                .flatMapToInt(String::chars) //IntStream
+                .mapToObj(c -> (char) c)     //Stream<Character>
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(charFrequency2);
+        ;//-----------------------------------------------------------------------------------
+        //For Tests
+//        Map<Integer, Map<Integer, List<String>>> result = words.stream()
+//                .collect(Collectors.groupingBy(String::length))
+//                .entrySet().stream()
+//                .collect(Collectors.toMap(Map.Entry::getKey,e -> Collectors.toMap(e.getValue().size(),e.getValue())));
+//        System.out.println(result);
+//
+//        System.out.println(words.stream()
+//                .collect(
+//                        Collectors.groupingBy(Function.identity(), Collectors.counting())));
+    }
+
+}
+
+//.collect(Collectors.toMap(Map.Entry::getKey, e -> Map.of(e.getValue().size(), e.getValue())));
